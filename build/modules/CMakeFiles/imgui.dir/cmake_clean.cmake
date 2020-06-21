b13ce7553fb5f6243d364d20e2d32a4686d92243
Yanuar T. Aditya Nugraha <yanuart.adityan@gmail.com>
2020-06-21 11:25:00 +0200

    include path for src and modules works except GLAD

---
+++ build/modules/CMakeFiles/imgui.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgui.dir/imgui/imgui.cpp.o"
+  "CMakeFiles/imgui.dir/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/imgui.dir/imgui/imgui_demo.cpp.o"
+  "libimgui.pdb"
+  "libimgui.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imgui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/imgui-glfw-glad-project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for imgui-glfw-glad-project.
+# This may be replaced when dependencies are built.
