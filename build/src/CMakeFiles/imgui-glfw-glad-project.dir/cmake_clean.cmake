cce256d8e7e0a34031beeeea6b76b6a4a7639c67
Yanuar Tri Aditya Nugraha <yanuart.adityan@gmail.com>
2020-06-21 16:41:58 +0200

    added ./built to gitignore

--- build/src/CMakeFiles/imgui-glfw-glad-project.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "imgui-glfw-glad-project.pdb"
-  "imgui-glfw-glad-project"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/imgui-glfw-glad-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/src/CMakeFiles/imgui-glfw-glad-project.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for imgui-glfw-glad-project.
-# This may be replaced when dependencies are built.
