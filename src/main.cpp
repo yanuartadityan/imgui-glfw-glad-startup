8fba37b6de0c3ccc6cbacba956b6f56d9d424000
Yanuar Tri Aditya Nugraha <yanuart.adityan@gmail.com>
2020-06-21 23:15:51 +0200

    working as import project in CLion

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#include <stdio.h>
+#include "examples/imgui_impl_glfw.h"
+#include "examples/imgui_impl_opengl3.h"
+#include <cstdio>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
